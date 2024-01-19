40f99774820f40ea5d7c60216abb73e34678df80
jonaslenz <38277721+jonaslenz@users.noreply.github.com>
2024-01-19 13:32:59 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import subprocess
+import requests
+import socket
+
+st.write("check server status")
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+result = sock.connect_ex(('127.0.0.1',5000))
+if result == 0:
+   status_flask = True
+else:
+    status_flask = False
+sock.close()
+
+
+stop_flask = False
+start_flask = False
+st.button("Refresh Server Status")
+
+if not status_flask:
+    st.write("Server is down.")
+    start_flask = st.button("Start the server")
+
+    if start_flask:
+        st.session_state.a = subprocess.Popen(["python", "hello1.py"], start_new_session = True, stdout = None, stdin=None, stderr= None)
+
+else:
+    st.write("Server is up.")
+
+    stop_flask = st.button("Stop the server")
+    r = requests.get('http://127.0.0.1:5000')
+    st.write(r)
+    if stop_flask:
+        st.session_state.a.terminate()
+        st.write("Flask stopped")
