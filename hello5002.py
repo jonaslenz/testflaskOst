2898e61ee0cceb7bb128d9bb1b22569f9e8df228
jonaslenz <38277721+jonaslenz@users.noreply.github.com>
2024-01-20 07:02:14 +0100

    Create hello5002.py

---
+++ hello5002.py
@@ -0,0 +1,29 @@
+# hello.py
+
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello, World from flask 5002!"
+
+
+incomes = [
+    { 'description': 'salary',
+      'amount': 5002 }
+]
+
+
+@app.route('/incomes')
+def get_incomes():
+    return flask.jsonify(incomes)
+
+
+@app.route('/incomes', methods=['POST'])
+def add_income():
+    incomes.append(flask.request.get_json())
+    return '', 204
+
+if __name__ == "__main__":
+    app.run(port=5002)
